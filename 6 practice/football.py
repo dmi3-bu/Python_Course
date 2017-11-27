353ce1764091bbaa5633645a49eba2d11cd0abff
fixen94 <fixen94@gmail.com>
2017-11-27 13:29:10 +1000

    Убран лишний временный скрипт

--- 6 practice/football.py
+++ 6 practice/football.py
@@ -7,20 +7,15 @@ except urllib.error.URLError:
     print('Ошибка: Соединение не установлено, проверьте подключение к Интернет')
     quit()
 print('Топ 5 чемпионатов:')
-counter = 1
-for championship in response:
-    if counter == 6: break
-    print(str(counter)+' '+championship['caption'])
+top_counter = 1
+for championship in response[:5]:
+    print(str(top_counter)+' '+championship['caption'])
     print("Лучшие команды:")
     team_url = url+'/'+str(championship['id'])+'/'+'leagueTable'
     teams_response = json.load(urllib.request.urlopen(team_url))
     teams = teams_response['standing']
     teams.sort(key=lambda team: team['goals'], reverse=True)
-    counter2 = 1
-    for team in teams:
-        if counter2 == 6:
-            break
+    for team in teams[:5]:
         print(team['teamName']+': '+str(team['goals'])+' голов')
-        counter2 += 1
     print()
-    counter += 1
+    top_counter += 1
