5cb1debd55a716abeb0b76c8fff464643e63b3aa
fixen94 <fixen94@gmail.com>
2018-01-14 20:46:03 +1000

    Добавлена 9-ая практика

---
+++ Django/Discography/musicapp/admin.py
@@ -0,0 +1,16 @@
+﻿from django.contrib import admin
+from musicapp.models import Artist, Album, Track, Genre
+
+
+# Модель админки, чтобы отображались все колонки при просмотре:
+class AllColumnsModelAdmin(admin.ModelAdmin):
+    def __init__(self, model, admin_site):
+        self.list_display = [field.name for field in model._meta.fields
+                             if field.name != "id"]
+        super(AllColumnsModelAdmin, self).__init__(model, admin_site)
+
+
+admin.site.register(Artist, AllColumnsModelAdmin)
+admin.site.register(Album, AllColumnsModelAdmin)
+admin.site.register(Track, AllColumnsModelAdmin)
+admin.site.register(Genre, AllColumnsModelAdmin)
---
+++ Django/Discography/musicapp/models.py
@@ -0,0 +1,58 @@
+﻿from django.db import models
+
+
+class Artist(models.Model):
+    artist_name = models.CharField('Исполнитель', max_length=30)
+    description = models.TextField('Описание', blank=True, null=True)
+    albums = models.ManyToManyField('Album', related_name='Artist',
+                                    blank=True)
+
+    class Meta:
+        ordering = ('artist_name',)
+        verbose_name = "Исполнитель"
+        verbose_name_plural = "Исполнители"
+
+    def __str__(self):
+        return self.artist_name
+
+
+class Album(models.Model):
+    album_name = models.CharField('Название', max_length=30)
+    release_date = models.DateField('Выпущен')
+    genres = models.ManyToManyField('Genre',  related_name='Album')
+    
+    class Meta:
+        ordering = ('album_name',)
+        verbose_name = 'Альбом'
+        verbose_name_plural = 'Альбомы'
+
+    def __str__(self):
+        return self.album_name
+
+
+class Track(models.Model):
+    track_name = models.CharField('Трек', max_length=40)
+    length = models.DurationField('Длительность')
+    order_no = models.IntegerField('Номер в альбоме')
+    album = models.ForeignKey('Album', related_name='Track',
+                              on_delete='SET_NULL')
+
+    class Meta:
+        ordering = ('album','order_no',)
+        verbose_name = "Трек"
+        verbose_name_plural = "Треки"
+
+    def __str__(self):
+        return self.track_name
+
+
+class Genre(models.Model):
+    name = models.CharField('Жанр', max_length=15)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
+
+    def __str__(self):
+        return self.name
